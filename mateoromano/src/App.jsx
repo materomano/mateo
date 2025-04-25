09dbe4295de4ddf1887cc35d7b1146129bcbdb63
mateo romano <mate.romano83@gmail.com>
2025-04-25 16:53:24 -0300

    mateo

--- mateoromano/src/App.jsx
+++ mateoromano/src/App.jsx
@@ -3,8 +3,9 @@ import Header from "./components/Header"
 import Estudios from "./components/Estudios"
 import React from "react"
 import Portada from "./components/Portada"
-
+import Proyectos from "./components/Proyectos"
 import Footer from "./components/Footer"
+import Sobremi from "./components/Sobremi.jsx"
 
 
 
@@ -22,12 +23,17 @@ function App() {
             <div id="portada">
               <Portada/>
             </div>
+            <div id="sobremi">
+            <Sobremi/>
+          </div>
           
           
           <div id="estudios">
                 <Estudios/>
            </div>
-          
+          <div id="proyectos">
+            <Proyectos/>
+          </div>
           <Footer/>
           
         </div>
